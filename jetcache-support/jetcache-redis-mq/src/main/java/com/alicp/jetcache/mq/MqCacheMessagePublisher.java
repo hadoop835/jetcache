b685a8aa82e279499dc1ff78c81eeae346155bc3
chenzhh <1>
2021-12-01 16:51:21 +0800

    采用广播更新缓存

---
+++ jetcache-support/jetcache-redis-mq/src/main/java/com/alicp/jetcache/mq/MqCacheMessagePublisher.java
@@ -0,0 +1,10 @@
+package com.alicp.jetcache.mq;
+
+import com.alicp.jetcache.support.CacheMessagePublisher;
+
+/**
+ * 缓存通过订阅方式更新缓存
+ */
+public interface MqCacheMessagePublisher extends CacheMessagePublisher {
+
+}
---
+++ jetcache-support/jetcache-redis-mq/src/main/java/com/alicp/jetcache/mq/impl/RocketmqCacheMessagePublisher.java
@@ -0,0 +1,15 @@
+package com.alicp.jetcache.mq.impl;
+
+import com.alicp.jetcache.mq.MqCacheMessagePublisher;
+import com.alicp.jetcache.support.CacheMessage;
+
+
+public class RocketmqCacheMessagePublisher implements MqCacheMessagePublisher {
+
+    @Override
+    public void publish(String area, String cacheName, CacheMessage cacheMessage) {
+
+    }
+
+
+}
